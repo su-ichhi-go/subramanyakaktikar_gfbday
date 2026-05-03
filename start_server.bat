f6a95cb2399d92360d22078a938a27b9be54ae87
su-ichhi-go <sdj150494@gmail.com>
2026-05-03 17:35:30 +0530

    Add files via upload

---
+++ start_server.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Starting local server for birthday website...
+echo.
+echo The website will be available at: http://localhost:8000/newww.html
+echo.
+echo Press Ctrl+C to stop the server
+echo.
+python -m http.server 8000
+pause
