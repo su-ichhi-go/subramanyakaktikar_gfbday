9a8bd816b325b897d2dde33c0fe22a7b1ed0993d
su-ichhi-go <sdj150494@gmail.com>
2026-05-07 15:22:13 +0530

    Update server script to use index.html

--- start_server.bat
+++ start_server.bat
@@ -1,7 +1,7 @@
 @echo off
 echo Starting local server for birthday website...
 echo.
-echo The website will be available at: http://localhost:8000/newww.html
+echo The website will be available at: http://localhost:8000/index.html
 echo.
 echo Press Ctrl+C to stop the server
 echo.
